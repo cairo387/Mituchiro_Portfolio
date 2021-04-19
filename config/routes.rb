65161f3314836953a15cc8c7574628e9e7b05230
cairo387 <mifuneri@outlook.jp>
2021-04-19 16:18:37 +0000

    [Add]deviseコントローラー,viewファイル

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :chiropractors
-  devise_for :users
+  devise_for :chiropractors, controllers: {
+  sessions: 'chiropractors/sessions',
+  passwords: 'chiropractors/passwords',
+  registrations: 'chiropractors/registrations'
+  }
+
+  devise_for :users, controllers: {
+  sessions: 'users/sessions',
+  passwords: 'users/passwords',
+  registrations: 'users/registrations'
+
+  }
+
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
