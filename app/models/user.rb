574a8ff4d9c594f68a1247cb2f3249721af0b6ed
cairo387 <mifuneri@outlook.jp>
2021-04-19 17:27:10 +0000

    [Add]整体師新規登録画面レイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #画像投稿用(refile)
+  attachment :image
+
+  enum sex: {
+    male: 0,
+    female: 1,
+    not_applicable: 2,
+  }
 end
