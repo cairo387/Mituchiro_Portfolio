923cbf6dd0f0a2dc005d9de18d29c9afc1da369b
cairo387 <mifuneri@outlook.jp>
2021-04-25 16:55:29 +0000

    [Add]施術内容登録ページ

--- app/controllers/admin/treatments_controller.rb
+++ app/controllers/admin/treatments_controller.rb
@@ -1,13 +1,41 @@
 class Admin::TreatmentsController < ApplicationController
-  def new
+  def index
+    @treatment = Treatment.new
+    @treatments = Treatment.all
   end
 
   def create
+    genre = Treatment.create(treatment_params)
+    if genre.save
+      redirect_to admin_treatments_path
+    else
+      @treatments = Treatment.all
+      render :index
+    end
   end
 
   def edit
+    @treatment = Treatment.find(params[:id])
   end
 
   def update
+    @treatment = Treatment.find(params[:id])
+    if @treatment.update(treatment_params)
+      redirect_to admin_treatments_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @treatment = Treatment.find(params[:id])
+    @treatment.destroy
+    redirect_to admin_treatments_path
+  end
+
+  private
+
+  def treatment_params
+  params.require(:treatment).permit(:name)
   end
 end
