70d9f22ac4accdab3d90c5dbc0de7fe3662349bc
cairo387 <mifuneri@outlook.jp>
2021-05-05 07:19:36 +0000

    [Add]バリデーションメッセージ記事投稿、予約にも

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,16 +1,16 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!
   def new
-    @wd = ["(日)", "(月)", "(火)", "(水)", "(木)", "(金)", "(土)"]
     @reservation = Reservation.new
     @reservation.start_date = Time.parse(params[:start])
     @reservation.end_date = Time.parse(params[:end])
     @reservation.chiropractor_id = params[:chiropractor_id]
+    @reservation.user_id = current_user.id
   end
 
   def confirm
-    @wd = ["(日)", "(月)", "(火)", "(水)", "(木)", "(金)", "(土)"]
     @reservation = Reservation.new(reservation_params)
+    render :new if @reservation.invalid?
   end
 
   def create
@@ -25,7 +25,6 @@ class ReservationsController < ApplicationController
       redirect_to reservations_completed_path
     else
       @reservation = Reservation.new(reservation_params)
-      @wd = ["(日)", "(月)", "(火)", "(水)", "(木)", "(金)", "(土)"]
       render :new
     end
   end
@@ -35,6 +34,6 @@ class ReservationsController < ApplicationController
 
   private
   def reservation_params
-    params.require(:reservation).permit(:name, :postal_code, :address, :phone_number, :station, :start_date, :end_date, :chiropractor_id)
+    params.require(:reservation).permit(:name, :postal_code, :address, :phone_number, :station, :start_date, :end_date, :chiropractor_id, :user_id)
   end
 end
